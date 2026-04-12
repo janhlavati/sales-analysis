6746c271a987d174f9af109dfcfe33278bb04600
janhlavati <janhlavaty11@gmail.com>
2026-04-12 14:37:28 +0200

    Highlighting the sales data

--- src/test.py
+++ src/test.py
@@ -9,7 +9,7 @@ print(data.isnull().sum())
 
 #Grouping the sales data by month/year
 data['month_year'] = data['order_date'].apply(lambda x: x.strftime('%Y-%m'))
-data_trend = data.groupby('month_year').sum(numeric_only = True)['sales'].reset_index()
+data_trend = data.groupby('month_year').sum(numeric_only=True)['sales'].reset_index()
 print(data_trend)
 
 #Visualizing the data in matplot
@@ -17,3 +17,13 @@ plt.figure(figsize=(15,6))
 plt.plot(data_trend['month_year'], data_trend['sales'])
 plt.xticks(rotation='vertical')
 plt.show()
+
+#Highlighting top 10 products by sales
+sales = pd.DataFrame(data.groupby('product_name').sum(numeric_only=True)['sales'])
+sales = sales.sort_values('sales', ascending=False)
+print(sales[:10])
+
+#Highlighting the most profitable categories
+profit = pd.DataFrame(data.groupby(['category', 'sub_category']).sum(numeric_only=True)['profit'])
+profit = profit.sort_values('profit', ascending=False)
+print(profit)
