d32500630ae367daf5725d510ec77d9cce0bc1a8
janhlavati <janhlavaty11@gmail.com>
2026-04-12 14:13:54 +0200

    Visualizing the data in matplotlib

--- src/test.py
+++ src/test.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 data = pd.read_excel('superstore_sales.xlsx')
 #print(data.to_string())
@@ -8,6 +9,11 @@ print(data.isnull().sum())
 
 #Grouping the sales data by month/year
 data['month_year'] = data['order_date'].apply(lambda x: x.strftime('%Y-%m'))
-data_trend = data.groupby('month_year').sum(numeric_only = True)['sales']
+data_trend = data.groupby('month_year').sum(numeric_only = True)['sales'].reset_index()
 print(data_trend)
 
+#Visualizing the data in matplot
+plt.figure(figsize=(15,6))
+plt.plot(data_trend['month_year'], data_trend['sales'])
+plt.xticks(rotation='vertical')
+plt.show()
