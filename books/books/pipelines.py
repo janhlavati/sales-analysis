a04be6e50f645368f78599784c152a407e1d77c6
janhlavati <janhlavaty11@gmail.com>
2026-04-18 17:07:49 +0200

    Configured the mongo pipeline

--- books/books/pipelines.py
+++ books/books/pipelines.py
@@ -11,6 +11,24 @@ import pymongo
 class MongoPipeline:
     COLLECTION_NAME = "books"
 
-class BooksPipeline:
+    def __init__(self, mongo_uri, mongo_db):
+        self.mongo_uri = mongo_uri
+        self.mongo_db = mongo_db
+
+    @classmethod
+    def from_crawlers(cls, crawler):
+        return cls(
+            mongo_uri=crawler.settings.get("MONGO_URI"),
+            mongo_db=crawler.settings.get("MONGO_DB"),
+        )
+
+    def open_spider(self, spider):
+        self.client=pymongo.MongoClient(self.mongo_uri)
+        self.db=pymongo.MongoClient[self.mongo_db]
+
+    def close_spider(self, spider):
+        self.client.close()
+
     def process_item(self, item, spider):
+        self.db[self.COLLECTION_NAME].insert_one(ItemAdapter(item).asdict())
         return item
