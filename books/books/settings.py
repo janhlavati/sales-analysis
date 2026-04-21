43b65eb84da74d9a43e91786ed7b2080091b7e61
janhlavati <janhlavaty11@gmail.com>
2026-04-21 18:13:41 +0200

    Deleted the initial commits in settings

--- books/books/settings.py
+++ books/books/settings.py
@@ -1,12 +1,3 @@
-# Scrapy settings for books project
-#
-# For simplicity, this file contains only settings considered important or
-# commonly used. You can find more settings consulting the documentation:
-#
-#     https://docs.scrapy.org/en/latest/topics/settings.html
-#     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-
 ITEM_PIPELINES = {
     "books.pipelines.MongoPipeline": 300,
 }
